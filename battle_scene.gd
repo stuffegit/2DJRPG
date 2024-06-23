f3cefe43979ff7507e7f2f600b3c245a052e30fe
Stf <136003236+stuffegit@users.noreply.github.com>
2024-06-23 13:27:23 +0200

    Battle meta-functionality fully functional for attack command, and exists into game-scene.

--- battle_scene.gd
+++ battle_scene.gd
@@ -16,14 +16,12 @@ var current_state
 var index: int = 0
 
 var player_team = Array()
-var player_turn_order = Array()
-
-var enemy_team = Array()
 var enemy_turn_order = Array()
 
 var playerturn_damage
 var enemyturn_damage
 var playertarget
+var attacked = false
 @onready var resetfocus = $Player/focus
 
 @onready var choice = $"CanvasLayer/choice"
@@ -40,22 +38,35 @@ func _ready():
 		enemies[i].initialize_monster_stats()
 		enemies[i]._update_progress_bar_enemies()
 	_handle_states(BATTLE_STATES.PLAYER_TURN)
+
 	
 	for i in range(0, 2):
 		player_team.push_back(enemies[i])
 		
+
 func _handle_states(new_state):
 	current_state = new_state
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.1).timeout
 	
 	match current_state:
 		BATTLE_STATES.PLAYER_TURN:
-			%GameStateLabel.text = "PLAYER_TURN"
-			print("player turn start")
-			show_choice()
+			enemies = $EnemyGroup.get_children()
+			if player.HP > 0:
+				if enemies.size() > 0:
+					%GameStateLabel.text = "PLAYER_TURN"
+					print("player turn start")
+					show_choice()
+					attacked = false
+				else:
+					hide_choice()
+					_handle_states(BATTLE_STATES.WIN)
+			else:
+				hide_choice()
+				_handle_states(BATTLE_STATES.LOSE)
 		BATTLE_STATES.ENEMY_TURN:
 			hide_choice()
-			enemy_turn_order = enemy_team
+			enemies = $EnemyGroup.get_children()
+			enemy_turn_order = enemies
 			%GameStateLabel.text = "ENEMY_TURN"
 			print("enemy turn start")
 			await get_tree().create_timer(0.1).timeout # enemy_turn_order is 0 without this
@@ -63,34 +74,41 @@ func _handle_states(new_state):
 			_handle_enemy_turn()
 		BATTLE_STATES.WIN:
 			%GameStateLabel.text = "WIN"
+			await get_tree().create_timer(2).timeout
+			get_tree().change_scene_to_file("res://game.tscn")
+			
 		BATTLE_STATES.LOSE:
 			%GameStateLabel.text = "LOSE"
 
 func _handle_enemy_turn():
 	print("handle enemy turn")
-	if enemy_turn_order.size() < 0:
-		show_choice()
-		_handle_states(BATTLE_STATES.PLAYER_TURN)
-		
-	else:
-		for i in range(enemies.size()):
-			print("enemy turn order: "+str(i+1))
-			enemies[i].Damage = int(rng.randf_range(enemies[i].Attack*0.8, enemies[i].Attack*1.2))
-			$Player.take_damage(enemies[i].Damage)
-			await get_tree().create_timer(1).timeout
-			enemy_turn_order.pop_front()
-		_handle_states(BATTLE_STATES.PLAYER_TURN)
-
+	while enemy_turn_order.size() > 0:
+		print("enemy_turn_order 0: "+str(enemy_turn_order.front()))
+		print("enemy turn order: "+str(enemy_turn_order.size()))
+		enemy_turn_order.front().Damage = int(rng.randf_range(enemy_turn_order.front().Attack*0.8, enemy_turn_order.front().Attack*1.2))
+		$Player.take_damage(enemy_turn_order.front().Damage)
+		await get_tree().create_timer(1).timeout
+		enemy_turn_order.pop_front()
+	show_choice()
+	_handle_states(BATTLE_STATES.PLAYER_TURN)
 
 func _attack_command(target):
+	attacked = true
 	$Player/focus.hide()
 	player.Damage = int(rng.randf_range(player.Attack*0.8, player.Attack*1.2))
 	enemies[target].take_damage(player.Damage, target)
 	await get_tree().create_timer(1).timeout
-	_handle_states(BATTLE_STATES.ENEMY_TURN)
+	if enemies[target].HP <= 0:
+		enemies[target].queue_free()
+		enemy_turn_order.remove_at(target)
+		await get_tree().create_timer(0.1).timeout
+	if enemies.size() > 0:
+		_handle_states(BATTLE_STATES.ENEMY_TURN)
+	else: 
+		_handle_states(BATTLE_STATES.WIN)
 
 func _process(_delta):
-	if not choice.visible:
+	if not choice.visible and current_state == BATTLE_STATES.PLAYER_TURN and attacked == false:
 		if Input.is_action_just_pressed("ui_up"):
 			if index > 0:
 				index -= 1
@@ -112,9 +130,7 @@ func switch_focus(x,y):
 	for enemy in enemies:
 		enemy.unfocus()
 	enemies[x].focus()
-	#print("enemy"+str(enemies[x])+"focused in switch_focus func")
 	enemies[y].unfocus()
-	#print("enemy"+str(enemies[x])+"unfocused in switch_focus func")
 
 func show_choice():
 	choice.show()
@@ -127,16 +143,11 @@ func _reset_focus():
 	index = 0
 	for enemy in enemies:
 		enemy.unfocus()
-		#print("enemy"+str(enemy)+"focused in _reset_focus func")
 
 func _start_choosing():
-	#print("start choosing")
 	_reset_focus()
 	enemies[0].focus()
-	#print("enemy"+str(enemies)[0]+"focused in _start_choosing func")
-
 
 func _on_attack_pressed():
-	#print("on attack pressed")
 	choice.hide()
 	_start_choosing()
